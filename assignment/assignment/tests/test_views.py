90b1bfc25ea525f576df168c50fb3317fb10fb01
Artemiy Rodionov <wertins71@gmail.com>
2020-03-12 11:47:19 +0300

    Show calculated total on POST

--- assignment/assignment/tests/test_views.py
+++ assignment/assignment/tests/test_views.py
@@ -8,3 +8,15 @@ class TestTomCalculator(TestCase):
 
     def test_form_presence(self):
         self.assertContains(self.client.get('/'), CalculationForm())
+
+    def test_calculation(self):
+        state, percent = percents.TAXES[0]
+        resp = self.client.post(
+            '/', {'price': 1, 'quantity': 1, 'state': state},
+        )
+
+        with_tax = logic.tax(1, percent)
+        self.assertEqual(resp.context['total']['discounted'], 1.0)
+        self.assertEqual(resp.context['total']['tax'], with_tax)
+        self.assertContains(resp, '1.00')
+        self.assertContains(resp, with_tax)
--- assignment/assignment/views.py
+++ assignment/assignment/views.py
@@ -7,6 +7,12 @@ from assignment.logic import calculate
 def calculator(request):
     ctx = {}
 
-    form = CalculationForm()
+    if request.method == 'POST':
+        form = CalculationForm(request.POST)
+        if form.is_valid():
+            ctx.update(calculate(**form.cleaned_data))
+    else:
+        form = CalculationForm()
+
     ctx['form'] = form
     return render(request, 'index.html', ctx)
